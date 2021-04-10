e5884d4a1b7214419374282aa4a202ad49bd6962
Jan Meier <jan@meier.wtf>
2021-04-10 11:32:15 +0200

    Adds TariffCalculator Class

---
+++ src/main/java/wtf/meier/tariff/interpreter/TariffCalculator.kt
@@ -0,0 +1,23 @@
+package wtf.meier.tariff.interpreter
+
+import wtf.meier.tariff.interpreter.model.Tariff
+import java.util.*
+
+
+interface Calculator {
+    fun calculate(tariff: Tariff, start: Date, end: Date): Receipt
+}
+
+data class Receipt(
+    val price: Int,
+    val currency: Currency,
+    val log: List<String> = emptyList()
+)
+
+class TariffCalculator : Calculator {
+
+    override fun calculate(tariff: Tariff, start: Date, end: Date): Receipt {
+        return tariff.calculate(start, end)
+    }
+
+}
