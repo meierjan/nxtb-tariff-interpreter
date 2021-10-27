9b559533a04f23f0a844a4a87c23c3e1b65d5921
Richard Zimmermann <rz65xuji@studserv.uni-leipzig.de>
2021-10-27 16:29:16 +0200

    add TariffDeserializer

---
+++ src/main/java/wtf/meier/tariff/interpreter/extension/customSerializer/TariffDeserializer.kt
@@ -0,0 +1,37 @@
+package wtf.meier.tariff.interpreter.extension.customSerializer
+
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import kotlinx.serialization.modules.subclass
+import wtf.meier.tariff.interpreter.model.rate.FixedRate
+import wtf.meier.tariff.interpreter.model.rate.Rate
+import wtf.meier.tariff.interpreter.model.rate.TimeBasedRate
+import wtf.meier.tariff.interpreter.model.tariff.DayBasedTariff
+import wtf.meier.tariff.interpreter.model.tariff.SlotBasedTariff
+import wtf.meier.tariff.interpreter.model.tariff.Tariff
+import wtf.meier.tariff.interpreter.model.tariff.TimeBasedTariff
+
+object TariffDeserializer {
+
+    private val module = SerializersModule {
+        polymorphic(Rate::class) {
+            subclass(FixedRate::class)
+            subclass(TimeBasedRate::class)
+        }
+        polymorphic(Tariff::class) {
+            subclass(SlotBasedTariff::class)
+            subclass(DayBasedTariff::class)
+            subclass(TimeBasedTariff::class)
+        }
+    }
+
+    private val format = Json {
+        serializersModule = module
+        classDiscriminator = "type"
+    }
+
+    fun deserializeTariff(serializedTariff: String): Tariff = format.decodeFromString(serializedTariff)
+
+}
