56b0cb534980719d58ba4125047cf5efba0bad0d
Richard Zimmermann <rz65xuji@studserv.uni-leipzig.de>
2021-10-29 17:46:27 +0200

    add start end timestamp to position

--- src/main/java/wtf/meier/tariff/interpreter/model/Receipt.kt
+++ src/main/java/wtf/meier/tariff/interpreter/model/Receipt.kt
@@ -1,5 +1,6 @@
 package wtf.meier.tariff.interpreter.model
 
+import java.time.Instant
 import java.util.*
 
 data class Receipt(
@@ -10,9 +11,12 @@ data class Receipt(
     val price: Int by lazy {
         positions.sumBy { it.price.credit }
     }
+    // TODO make start and end non-optional
 
     data class Position(
         val price: Price,
-        val description: String
+        val description: String,
+        val positionStart: Instant? = null,
+        val positionEnd: Instant? = null
     )
 }
