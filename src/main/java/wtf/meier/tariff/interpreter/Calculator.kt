3ff9aa682b9fc92d9c86ab16ea8b41877d634eed
Richard Zimmermann <rz65xuji@studserv.uni-leipzig.de>
2021-10-29 17:38:12 +0200

    calculate freeMinutes before tariff calculation

--- src/main/java/wtf/meier/tariff/interpreter/Calculator.kt
+++ src/main/java/wtf/meier/tariff/interpreter/Calculator.kt
@@ -1,9 +1,10 @@
 package wtf.meier.tariff.interpreter
 
+import wtf.meier.tariff.interpreter.extension.RentalPeriod
 import wtf.meier.tariff.interpreter.model.Receipt
+import wtf.meier.tariff.interpreter.model.tariff.FreeMinutesCalculator
 import wtf.meier.tariff.interpreter.model.tariff.Tariff
 import wtf.meier.tariff.interpreter.model.tariff.TariffCalculator
-import java.time.Instant
 
 
 class Calculator(
@@ -11,7 +12,10 @@ class Calculator(
 ) : ICalculator {
 
 
-    override fun calculate(tariff: Tariff, start: Instant, end: Instant): Receipt =
-        calculator.calculate(tariff, start, end)
+    override fun calculate(tariff: Tariff, rentalPeriod: RentalPeriod): Receipt =
+
+        calculator.calculate(
+            tariff, FreeMinutesCalculator.calculateFreeMinutes(tariff, rentalPeriod)
+        )
 
 }
