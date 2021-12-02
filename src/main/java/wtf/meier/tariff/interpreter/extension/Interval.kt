153d6a7e15b46f9ae36628a8004bd762b3f449ba
Richard Zimmermann <rz65xuji@studserv.uni-leipzig.de>
2021-12-02 15:38:35 +0100

    move minius operator to extention

--- src/main/java/wtf/meier/tariff/interpreter/extension/Interval.kt
+++ src/main/java/wtf/meier/tariff/interpreter/extension/Interval.kt
@@ -1,5 +1,12 @@
 package wtf.meier.tariff.interpreter.extension
 
 import wtf.meier.tariff.interpreter.model.Interval
+import java.util.concurrent.TimeUnit
 
 fun Interval.durationMillis(): Long = timeUnit.toMillis(timeAmount.toLong())
+
+operator fun Interval.minus(start: Interval): Interval = Interval(
+    (timeUnit.toMillis(timeAmount.toLong()) - start.timeUnit.toMillis(start.timeAmount.toLong())).toInt(),
+    TimeUnit.MILLISECONDS
+)
+
