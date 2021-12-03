8a27190ce3462d93f2bd97071f1a8337391bbee4
Richard Zimmermann <rz65xuji@studserv.uni-leipzig.de>
2021-12-03 16:23:10 +0100

    add InconsistentCurrencyException

--- src/main/java/wtf/meier/tariff/interpreter/model/extension/CalculatedPrice.kt
+++ src/main/java/wtf/meier/tariff/interpreter/model/extension/CalculatedPrice.kt
@@ -1,12 +1,12 @@
 package wtf.meier.tariff.interpreter.model.extension
 
-import wtf.meier.tariff.interpreter.exception.InconsitantCurrencyException
+import wtf.meier.tariff.interpreter.exception.InconsistentCurrencyException
 import wtf.meier.tariff.interpreter.model.Receipt
 import wtf.meier.tariff.interpreter.model.rate.RateCalculator
 import java.util.*
 
 fun List<RateCalculator.CalculatedPrice>.toReceipt(currency: Currency): Receipt {
-    this.map { if (it.currency !== currency) throw InconsitantCurrencyException("The tariff and rate currencies are not compatible")}
+    this.map { if (it.currency !== currency) throw InconsistentCurrencyException("The tariff and rate currencies are not compatible")}
     return Receipt(
         positions = this.map {
             Receipt.Position(
