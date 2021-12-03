c3f6ae9c1c3a58713a041cd616aa9e697e9a0740
Richard Zimmermann <rz65xuji@studserv.uni-leipzig.de>
2021-12-03 16:14:08 +0100

    add FreeMinutesCalculator interface

--- src/main/java/wtf/meier/tariff/interpreter/FreeMinutesCalculator.kt
+++ src/main/java/wtf/meier/tariff/interpreter/FreeMinutesCalculator.kt
@@ -1,25 +1,24 @@
-package wtf.meier.tariff.interpreter.model.tariff
+package wtf.meier.tariff.interpreter
 
-import wtf.meier.tariff.interpreter.extension.RentalPeriod
+import wtf.meier.tariff.interpreter.model.RentalPeriod
 import wtf.meier.tariff.interpreter.extension.minus
 import wtf.meier.tariff.interpreter.extension.plus
 import wtf.meier.tariff.interpreter.model.Interval
 import wtf.meier.tariff.interpreter.model.Price
-import wtf.meier.tariff.interpreter.model.Receipt
 import wtf.meier.tariff.interpreter.model.rate.RateCalculator
+import wtf.meier.tariff.interpreter.model.tariff.Tariff
 import java.time.Instant
 import java.util.concurrent.TimeUnit
 
-object FreeMinutesCalculator {
+object FreeMinutesCalculator:IFreeMinutesCalculator {
 
-    fun calculateFreeMinutes(tariff: Tariff, rentalPeriod: RentalPeriod): RentalPeriod {
+    override fun calculateFreeMinutes(tariff: Tariff, rentalPeriod: RentalPeriod): RentalPeriod {
 
         var period = deductGoodWillMinutes(tariff, rentalPeriod)
         period = deductFreeMinutes(tariff, period)
 
         return period
     }
-
 
     private fun deductGoodWillMinutes(
         tariff: Tariff,
@@ -28,13 +27,13 @@ object FreeMinutesCalculator {
 
         if (!tariff.fairTariff) return rentalPeriod
 
-        val durationInSeconds: Long = rentalPeriod.calculateDuration().epochSecond
+        val durationInSeconds: Long = rentalPeriod.duration.epochSecond
 
         val freeMinutes: Long = if (durationInSeconds <= 60 * 10) {
             return rentalPeriod
-        } else if (durationInSeconds > 60 * 10 && durationInSeconds <= 60 * 60) {
+        } else if (durationInSeconds <= 60 * 60) {
             5
-        } else if (durationInSeconds > 60 * 60 && durationInSeconds <= 60 * 60 * 24) {
+        } else if (durationInSeconds <= 60 * 60 * 24) {
             10
         } else {
             45
@@ -43,36 +42,31 @@ object FreeMinutesCalculator {
         val goodWillPosition = RateCalculator.CalculatedPrice(
             price = Price(0),
             currency = tariff.rates.first().currency,
-            positionStart = rentalPeriod.calculatedEnd - Instant.ofEpochSecond(TimeUnit.MINUTES.toSeconds(freeMinutes)),
-            positionEnd = rentalPeriod.calculatedEnd,
+            calculationStart = rentalPeriod.calculatedEnd - Instant.ofEpochSecond(TimeUnit.MINUTES.toSeconds(freeMinutes)),
+            calculationEnd = rentalPeriod.calculatedEnd,
             description = "$freeMinutes minutes were deducted from your travel time as a gesture of goodwill"
         )
 
-        rentalPeriod.calculatedEnd = rentalPeriod.calculatedEnd - Instant.ofEpochSecond(TimeUnit.MINUTES.toSeconds(freeMinutes))
         rentalPeriod.positions.add(goodWillPosition)
 
-        return rentalPeriod
+        return rentalPeriod.copy(calculatedEnd = rentalPeriod.calculatedEnd - Instant.ofEpochSecond(TimeUnit.MINUTES.toSeconds(freeMinutes)))
     }
-
 
     private fun deductFreeMinutes(tariff: Tariff, rentalPeriod: RentalPeriod): RentalPeriod {
         if (tariff.freeSeconds <= 0) return rentalPeriod
-        val durationInSeconds: Long = rentalPeriod.calculateDuration().epochSecond
+        val durationInSeconds: Long = rentalPeriod.duration.epochSecond
         val freeSeconds = minOf(durationInSeconds.toInt(), tariff.freeSeconds)
 
         val freeMinutesPosition = RateCalculator.CalculatedPrice(
             price = Price(0),
             currency = tariff.rates.first().currency,
-            positionStart = rentalPeriod.calculatedStart,
-            positionEnd = rentalPeriod.calculatedStart + Interval(freeSeconds, TimeUnit.SECONDS),
+            calculationStart = rentalPeriod.calculatedStart,
+            calculationEnd = rentalPeriod.calculatedStart + Interval(freeSeconds, TimeUnit.SECONDS),
             description = "${TimeUnit.SECONDS.toMinutes(freeSeconds.toLong())} minutes were deducted from your travel time as a gesture of goodwill"
         )
 
-        rentalPeriod.calculatedStart = rentalPeriod.calculatedStart + Interval(freeSeconds, TimeUnit.SECONDS)
         rentalPeriod.positions.add(freeMinutesPosition)
 
-        return rentalPeriod
+        return rentalPeriod.copy(calculatedStart = rentalPeriod.calculatedStart + Interval(freeSeconds, TimeUnit.SECONDS))
     }
-
-
 }
