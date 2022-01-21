637e8a56350a500e5260a99f2c23002c373f6a79
richard <rzimmermann@nextbike.com>
2022-01-21 17:57:14 +0100

    removed DayBasedTariff

--- src/main/java/wtf/meier/tariff/interpreter/model/tariff/TariffCalculator.kt
+++ src/main/java/wtf/meier/tariff/interpreter/model/tariff/TariffCalculator.kt
@@ -2,20 +2,17 @@ package wtf.meier.tariff.interpreter.model.tariff
 
 import wtf.meier.tariff.interpreter.model.RentalPeriod
 import wtf.meier.tariff.interpreter.model.Receipt
-import wtf.meier.tariff.interpreter.model.tariff.calculator.DayBasedTariffCalculator
 import wtf.meier.tariff.interpreter.model.tariff.calculator.SlotBasedTariffCalculator
 import wtf.meier.tariff.interpreter.model.tariff.calculator.TimeBasedTariffCalculator
 
 class TariffCalculator(
     private val slotBasedTariffCalculator: SlotBasedTariffCalculator = SlotBasedTariffCalculator(),
     private val timeBasedTariffCalculator: TimeBasedTariffCalculator = TimeBasedTariffCalculator(),
-    private val dayBasedTariffCalculator: DayBasedTariffCalculator = DayBasedTariffCalculator()
 ) {
 
     fun calculate(tariff: Tariff, rentalPeriod: RentalPeriod): Receipt =
         when (tariff) {
             is SlotBasedTariff -> slotBasedTariffCalculator.calculate(tariff, rentalPeriod)
             is TimeBasedTariff -> timeBasedTariffCalculator.calculate(tariff, rentalPeriod)
-            is DayBasedTariff -> dayBasedTariffCalculator.calculate(tariff, rentalPeriod)
         }
 }
