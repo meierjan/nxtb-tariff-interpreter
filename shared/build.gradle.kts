78d2b843bef732cd2293bb943dbba9d39701555d
Jan Meier <jan@meier.wtf>
2024-01-11 12:56:55 +0800

    FREE: Prepare for publishing on sonatype

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,8 +1,18 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.kotlinBinaryCompatibilityValidator)
+    alias(libs.plugins.kotlinKover)
+    alias(libs.plugins.dokka)
+    id("maven-publish")
+    id("signing")
 }
+
+group = "wtf.meier.tariffinterpreter"
+version = "0.1.0"
 
 kotlin {
     androidTarget {
@@ -12,11 +22,9 @@ kotlin {
             }
         }
     }
-    
+
     listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
+        iosX64(), iosArm64(), iosSimulatorArm64()
     ).forEach {
         it.binaries.framework {
             baseName = "shared"
@@ -33,11 +41,6 @@ kotlin {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
-        jvmTest {
-//            implementation(libs.hamkrest)
-//            implementation("org.junit.jupiter:junit-jupiter:5.8.2")
-
-        }
     }
 }
 
@@ -47,4 +50,72 @@ android {
     defaultConfig {
         minSdk = 24
     }
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+
+val javadocJar = tasks.register<Jar>("javadocJar") {
+//    dependsOn(tasks)
+    archiveClassifier.set("javadoc")
+    from("$buildDir/dokka")
+}
+
+val repoUrl = "https://github.com/meierjan/nxtb-tariff-interpreter"
+extensions.configure<PublishingExtension> {
+    publications {
+        withType<MavenPublication> {
+            artifact(javadocJar)
+
+            pom {
+                name.set("nextbike tariff interpreter")
+                description.set("A library to interpret nextbike tariffs and to calculate their prices")
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://opensource.org/licenses/Apache-2.0")
+                    }
+                }
+                url.set(repoUrl)
+                issueManagement {
+                    system.set("Github")
+                    url.set("${repoUrl}/issues")
+                }
+                scm {
+                    connection.set("$repoUrl.git")
+                    url.set(repoUrl)
+                }
+                developers {
+                    developer {
+                        name.set("Jan Meier")
+                        email.set("tariff-interpreter@meier.dev")
+                    }
+                }
+            }
+        }
+    }
+
+
+    val publishing = extensions.getByType<PublishingExtension>()
+    extensions.configure<SigningExtension> {
+        useInMemoryPgpKeys(
+            gradleLocalProperties(rootDir).getProperty("gpgKeySecret"),
+            gradleLocalProperties(rootDir).getProperty("gpgKeyPassword"),
+        )
+
+        sign(publishing.publications)
+    }
+
+    // TODO: remove after https://youtrack.jetbrains.com/issue/KT-46466 is fixed
+    project.tasks.withType(AbstractPublishToMaven::class.java).configureEach {
+        dependsOn(project.tasks.withType(Sign::class.java))
+    }
+
+}
+
+koverMerged {
+    enable()
 }
